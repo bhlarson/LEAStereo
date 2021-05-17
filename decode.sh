e3e474703e5ba4009832908dac8af02188e63b03
Brad Larson <brad.larson1@thermofisher.com>
2021-05-17 08:30:06 -0700

    Run parameter changes

--- decode.sh
+++ decode.sh
@@ -1,2 +1,2 @@
 python decode.py --dataset sceneflow --step 3 \
---resume ./run/sceneflow/experiment_3/checkpoint_0.pth.tar
+--resume ./run/sceneflow/model_best.pth.tar
--- train_sf.sh
+++ train_sf.sh
@@ -1,15 +1,15 @@
-python3 train.py --batch_size=4 \
+CUDA_VISIBLE_DEVICES=0 python3 train.py --batch_size=4 \
                 --crop_height=384 \
                 --crop_width=576 \
                 --maxdisp=192 \
                 --threads=8 \
-                --save_path='./run/sceneflow/experiment_3/checkpoint_0/retrain/' \
+                --save_path='./run/sceneflow/retrain/' \
                 --fea_num_layer 6 --mat_num_layers 12 \
                 --fea_filter_multiplier 8 --fea_block_multiplier 4 --fea_step 3  \
                 --mat_filter_multiplier 8 --mat_block_multiplier 4 --mat_step 3  \
-                --net_arch_fea='./run/sceneflow/experiment_3/checkpoint_0/feature_network_path.npy' \
-                --cell_arch_fea='run/sceneflow/experiment_3/checkpoint_0/feature_genotype.npy' \
-                --net_arch_mat='run/sceneflow/experiment_3/checkpoint_0/matching_network_path.npy' \
-                --cell_arch_mat='run/sceneflow/experiment_3/checkpoint_0/matching_genotype.npy' \
-                --nEpochs=20 2>&1 |tee ./run/sceneflow/experiment_3/checkpoint_0/retrain/log.txt
+                --net_arch_fea='./run/sceneflow/feature_network_path.npy' \
+                --cell_arch_fea='run/sceneflow/feature_genotype.npy' \
+                --net_arch_mat='run/sceneflow/matching_network_path.npy' \
+                --cell_arch_mat='run/sceneflow/matching_genotype.npy' \
+                --nEpochs=20 2>&1 |tee ./run/sceneflow/retrain/log.txt
 
